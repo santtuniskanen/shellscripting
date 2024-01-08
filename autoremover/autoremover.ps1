c8dedd16d93d1dcc34e05bea429c3483083756b2
santtuniskanen <santtuniska02@gmail.com>
2024-01-08 23:34:42 +0200

    work on autoremover

--- autoremover/autoremover.ps1
+++ autoremover/autoremover.ps1
@@ -1,11 +1,12 @@
-# Get the current username
+# Get the current username to make sure
+# it would work on another Windows machine easier
 $currentUsername = $env:USERNAME
 
-# Define the path to the folder using the variable
+# Defining the path to the directory using the variable
 $directoryPath = "C:\Users\$currentUsername\Documents\TEMP"
 
 # Making sure that the directory exists and then 
-# removing all contents of the folder
+# removing all contents of the folder if the condition is met
 if (Test-Path -Path $directoryPath -PathType Container) {
     Remove-Item -Path "$directoryPath\*" -Recurse -Force
     Write-Host "Contents in $directoryPath deleted successfully."
